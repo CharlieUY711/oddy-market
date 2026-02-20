5dc2a7b7d21e040e8d72369e95ebf501990280ec
Tu Nombre <tu@email.com>
2026-02-20 06:20:06 -0300

    Conectar PersonasView a Supabase

--- src/utils/supabase/info.ts
+++ src/utils/supabase/info.ts
@@ -1,4 +1,4 @@
-exp// Supabase connection info
+// Supabase connection info
 // These values are auto-populated when you connect to Supabase via Figma Make
 export const projectId = "qhnmxvexkizcsmivfuam";
 export const publicAnonKey = "sb_publishable_sB3F5T731PMqPXMOFq8pVg_Trg4AbeW";
